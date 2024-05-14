513d5637b9a79b80e6aaed98a10f8c9d2138dc74
jkhan94 <hjkwork@naver.com>
2024-05-14 10:42:15 +0900

    memo.java

--- src/main/java/com/sparta/spring_prepare/memo.java
+++ src/main/java/com/sparta/spring_prepare/memo.java
@@ -1,12 +1,15 @@
 package com.sparta.spring_prepare;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 // 그래들이 빌드할 떄 자동으로 만들어줌.
 @Getter
 @Setter
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Memo {
     private String username;
     private String contents;
+    private final String test;
 }
