00fd7387e67cfb0a489e5966dd4fdf0dd1b08b60
jkhan94 <hjkwork@naver.com>
2024-05-16 17:46:23 +0900

    객체 중복 생성 해결 - MemoControllder에선 MemoService, Memoservice에선 MemoRepository.

--- memo/src/main/java/com/sparta/memo/service/MemoService.java
+++ memo/src/main/java/com/sparta/memo/service/MemoService.java
@@ -9,10 +9,10 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import java.util.List;
 
 public class MemoService {
-    private final JdbcTemplate jdbcTemplate;
+    private final MemoRepository memoRepository;
 
     public MemoService(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+        this.memoRepository = new MemoRepository(jdbcTemplate);
     }
 
     public MemoResponseDto createMemo(MemoRequestDto requestDto) {
@@ -20,7 +20,6 @@ public class MemoService {
         Memo memo = new Memo(requestDto);
 
         // 디비에 요청 DTO를 변환한 엔티티 memo 전달
-        MemoRepository memoRepository  = new MemoRepository(jdbcTemplate);
         Memo saveMemo = memoRepository.save(memo);
 
         // Entity -> ResponseDto
@@ -31,12 +30,10 @@ public class MemoService {
 
     public List<MemoResponseDto> getMemos() {
         // DB 조회
-        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
         return memoRepository.findAll();
     }
 
     public Long updateMemo(Long id, MemoRequestDto requestDto) {
-        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
         // 해당 메모가 DB에 존재하는지 확인
         Memo memo = memoRepository.findById(id);
         if (memo != null) {
@@ -49,7 +46,6 @@ public class MemoService {
     }
 
     public Long deleteMemo(Long id) {
-        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
         // 해당 메모가 DB에 존재하는지 확인
         Memo memo = memoRepository.findById(id);
         if (memo != null) {
