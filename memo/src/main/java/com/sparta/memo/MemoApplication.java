fa9234353e3a2c4a1966128246ce380156633173
jkhan94 <hjkwork@naver.com>
2024-05-17 15:33:14 +0900

    JPA Auditing

--- memo/src/main/java/com/sparta/memo/MemoApplication.java
+++ memo/src/main/java/com/sparta/memo/MemoApplication.java
@@ -2,7 +2,9 @@ package com.sparta.memo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // 시간 자동 저장 기능 사용
 @SpringBootApplication
 public class MemoApplication {
 
